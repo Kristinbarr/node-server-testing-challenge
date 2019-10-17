b76f7b7990b174137d3fc995e5e8636f823ce8f2
Kristin Barr <kristinbarr@gmail.com>
2019-10-17 19:18:04 -0400

    resource model test

---
+++ resource/resource-model.spec.js
@@ -0,0 +1,9 @@
+// testing DB models
+const Resources = require('./resource-model')
+const db = require('../data/dbConfig')
+
+describe('findAll()', () => {
+  it('should return all resources', () => {})
+  it('should return all resources', () => {})
+  it('should return all resources', () => {})
+})
