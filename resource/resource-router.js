91699d6826ca35eaa834ca987183160b51172be4
Kristin Barr <kristinbarr@gmail.com>
2019-10-18 00:03:55 -0400

    completed delete route with error handling

--- resource/resource-router.js
+++ resource/resource-router.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res) => {
     const resources = await Resource.findAll()
     res.status(200).json(resources)
   } catch (error) {
-    res.status(500).json({ message: "could not retrieve resources" })
+    res.status(500).json({ message: 'could not retrieve resources' })
   }
 })
 
@@ -21,10 +21,13 @@ router.post('/', async (req, res) => {
   }
 })
 
-
-router.delete('/:id', (req, res) => {
-  return Resource.remove(req.params.id)
-  .where({id})
+router.delete('/:id', async (req, res) => {
+  try {
+    await Resource.remove(req.params.id)
+    res.status(200).json({ message: 'success deleting resource' })
+  } catch (error) {
+    res.status(500).json({ message: 'probem deleting resource' })
+  }
 })
 
 module.exports = router
